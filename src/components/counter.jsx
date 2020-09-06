b1ce47fff2e65dc4a917b09ff4de1684a1ec7f01
Usman <usman_9@outlook.com>
2020-09-06 15:42:44 +0500

    Rendering the classes dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,11 +7,18 @@ class Conuter extends Component {
   render() {
     return (
       <React.Fragment>
-        <spam>{this.formatCount()}</spam>
-        <button>Increment</button>
+        <spam className={this.getBadgeClasses()}>{this.formatCount()}</spam>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
