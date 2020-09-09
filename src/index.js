fd2d715d06f0ed0bdc53ce0ed835910fcf88360d
Usman <usman_9@outlook.com>
2020-09-09 22:08:50 +0500

    Navbar added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import Counters from "./components/counters";
 import * as serviceWorker from "./serviceWorker";
 import App from "./App";
 
